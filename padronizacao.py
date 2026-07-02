f6fde6230806666f5cd1dfe1a59da42fbeec92dd
J0ze-arch <josecarlos171005@gmail.com>
2026-07-01 23:34:33 -0300

    Adicionando script para RouterOS V7

--- padronizacao.py
+++ padronizacao.py
@@ -56,7 +56,6 @@ class MyScript(Script):
             'username': usuario_mikrotik,
             'password': senha,
             'port': int(porta),
-            'fast_cli': True,
         }
 
         arquivo_script = diretorio_pdrv7 if versao else diretorio_pdr
